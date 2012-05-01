890f5c1d6fe77bd4d0fa8dcc1b4e1b4103bc9d16
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2012-05-01 13:51:42 -0400

    ClaferIR project

---
+++ csharp/ClaferIR/ClaferIR/ClaferIR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using clafer.org.ir;
+
+namespace clafer.org.ir.ClaferIR
+{
+    public class ClaferIR
+    {
+        /// <summary>
+        /// Parses the XML IR representation.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public Module loadClaferModel(string fileName)
+        {
+            return Module.Load(fileName);
+        }
+    }
+}
---
+++ csharp/ClaferIR/ClaferIR/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClaferIR")]
+[assembly: AssemblyDescription("API for accessing the Intermediate Representation (IR) produced by the Clafer compiler.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Generative Software Development Lab")]
+[assembly: AssemblyProduct("ClaferIR")]
+[assembly: AssemblyCopyright("Copyright © Generative Software Development Lab 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29faf159-fa8a-4b33-a1fb-9a2477a0549b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
