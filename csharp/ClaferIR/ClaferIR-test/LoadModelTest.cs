a160bcad2494894c74561d57faf0bbccee14f82c
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2012-05-01 13:53:19 -0400

    ClaferIR-test project

---
+++ csharp/ClaferIR/ClaferIR-test/LoadModelTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using clafer.org.ir.ClaferIR;
+using clafer.org.ir;
+
+namespace ClaferIR_test
+{
+    /// <summary>
+    /// Loads XML IR of the model LoadModelTest.cfr
+    /// </summary>
+    [TestClass]
+    public class LoadModelTest
+    {
+        public LoadModelTest()
+        {
+            Module module = (new ClaferIR()).loadClaferModel("LoadMoredTest.xml");
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic	here
+            //
+        }
+    }
+}
